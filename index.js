983c223d956aee884e6dd49930ad08ec2122bfbd
Lisha Liang <lishaliang1999@gmail.com>
2022-08-13 17:23:23 +0000

    runfile

--- index.js
+++ index.js
@@ -1,31 +1,8 @@
 
- const { nextISSTimesForMyLocation } = require('./iss');
-
-//  fetchMyIP((error, ip) => {
-//    if (error) {
-//      console.log("It didn't work!" , error);
-//      return;
-//    }
- 
-//    console.log('It worked! Returned IP:' , ip);
-//  });
-
-
-
-// fetchCoordsByIP('104.171.50.69', (abc, {latitude, longitude}) => {
-//   console.log({'latitude': latitude, 'longitude': longitude});
-// })
-
-// fetchISSFlyOverTimes({latitude: '43.87316131591797', longitude: '-79.2615966796875'}, (error, data) => {
-//   if (error) {
-//     console.log("It didn't work!", error);
-//     return;
-//   }
-//   console.log(data)
-// })
+const { nextISSTimesForMyLocation } = require('./iss');
 
 const printPassTimes = function(passTimes) {
-  for (const pass of passTimes) {
+  for (const pass of passTimes.response) {
     const datetime = new Date(0);
     datetime.setUTCSeconds(pass.risetime);
     const duration = pass.duration;
@@ -37,6 +14,8 @@ nextISSTimesForMyLocation((error, passTimes) => {
   if (error) {
     return console.log("It didn't work!", error);
   }
-  // success, print out the deets!
+
   printPassTimes(passTimes);
+  //console.log(passTimes)
 });
+
